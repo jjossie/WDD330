a0dc63e928d61da59110ac9c853ed9cde15eb062
Joel <joeljossie@gmail.com>
2022-01-14 12:48:36 -0700

    added week 2, added styles, reorganized content

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@ const links = [
     {
         label: "Week 1 Notes",
         url: "week1/index.html"
+    },
+    {
+        label: "Week 1 Code Exercises",
+        url: "week1/exercises.js"
+    },
+    {
+        label: "Week 2 Code Exercises",
+        url: "week2/exercises.html"
     }
 ]
 
 function populateLinks(){
-    var linkList = document.getElementById("linkList");
+    let linkList = document.getElementById("linkList");
     links.forEach(link => {
-        linkList.innerHTML += "<li class='portfolioLink'>" + link.label + "</li>";
+        // linkList.innerHTML += "<li class='portfolioLink'>" + link.label + "</li>";
+        linkList.innerHTML += `<li class='portfolioLink'><a href='${link.url}'>${link.label}</a></li>`;
+
     });
 }
 
---
+++ week2/week2.js
@@ -0,0 +1,8 @@
+function quiz(){
+    const question = "What is Superman's real name?";
+    const answer = prompt(question);
+    alert(`You answered ${answer}`)
+}
+
+
+console.log(typeof NaN); 
