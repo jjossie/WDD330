0c4200c6b54fe8e34e904c6811197f645919d32c
Joel Jossie <joeljossie@gmail.com>
2022-02-09 21:48:54 -0700

    delete somewhat works

--- todo_app/main.js
+++ todo_app/main.js
@@ -7,25 +7,35 @@ import {TaskList} from "./todos.js"
 // document.getElementById("todo-item-list").appendChild(itemTwo.render())
 
 let list = new TaskList();
-list.addTask("pull up")
-list.addTask("drank")
-list.addTask("headshot")
-list.addTask("drank")
-list.addTask("stand up")
-list.addTask("drank")
-list.addTask("sit down")
-list.addTask("drank")
-list.addTask("wake up")
-list.addTask("drank")
-list.addTask("pass out")
-list.addTask("faded")
-list.addTask("drank")
-list.addTask("faded")
-list.addTask("drank")
+// list.addTask("pull up")
+// list.addTask("drank")
+// list.addTask("headshot")
+// list.addTask("drank")
+// list.addTask("stand up")
+// list.addTask("drank")
+// list.addTask("sit down")
+// list.addTask("drank")
+// list.addTask("wake up")
+// list.addTask("drank")
+// list.addTask("pass out")
+// list.addTask("faded")
+// list.addTask("drank")
+// list.addTask("faded")
+// list.addTask("drank")
 
-let parent = document.getElementById("todo-list");
-let footer = document.getElementById("todo-list-footer");
-parent.insertBefore(list.renderTaskList(), footer);
+// list.saveToStorage();
+
+list.loadFromStorage();
+
+list.deleteTask(12);
+
+function load(){
+    let parent = document.getElementById("todo-list");
+    let footer = document.getElementById("todo-list-footer");
+    parent.insertBefore(list.renderTaskList(), footer);
+}
+load();
+
 
 // class ToDoList {
 //     constructor(){
--- todo_app/todos.js
+++ todo_app/todos.js
@@ -1,41 +1,110 @@
-export default class Task {
+class Task {
+    static nextId = 0;
     constructor(name) {
+        this.id = ++Task.nextId;
         this.name = name;
         this.completed = false;
     }
+    static loadFromObject(simpleObject) {
+        var newTask = new Task();
+        newTask.id = simpleObject.id;
+        newTask.name = simpleObject.name;
+        newTask.completed = simpleObject.completed;
+        return newTask;
+    }
     complete() {
         this.completed = true;
+        this.save();
     }
     incomplete() {
         this.completed = false;
+        this.save();
     }
-
+    save() {
+        let storage = window.localStorage;
+        storage.setItem(this.id, JSON.stringify(this));
+    }
+    delete() {
+        const taskHTML = document.getElementById(`${this.id}-task`);
+        if (taskHTML == null)
+            return;
+        taskHTML.remove();
+        window.localStorage.removeItem(this.id);
+    }
     render() {
         let taskHTML = document.createElement("li");
+        taskHTML.id = `${this.id}-task`;
         taskHTML.classList.add("todo-item");
         taskHTML.classList.add("box");
         taskHTML.innerHTML = `
             <input type="checkbox" class="todo-check"${this.completed ? "checked" : ""}>
-            <span class="todo-name">${this.name}</span>
+            (${this.id})<span class="todo-name">${this.name}</span>
             <button class="todo-delete">X</button>
         `;
-        
+
         return taskHTML;
     }
 }
 
 export class TaskList {
-    constructor(){
+    constructor() {
         this.tasks = [];
+        this.taskIds = [];
     }
-    addTask(name){
+    /**
+     * Creates a Task object, adds it to this taskList, and saves it.
+     * @param {string} name 
+     */
+    addTask(name) {
         let task = new Task(name);
         this.tasks.push(task);
+        task.save();
     }
-    renderTaskList(){
+    /**
+     * Finds a task with a given ID and removes it both from this 
+     * TaskList and from localStorage.
+     * @param {*} id the ID of the task to be removed
+     */
+    deleteTask(id) {
+        let task = this.tasks.find((task) => {
+            console.log(`looking for ${id} - task id: ${task.id}`);
+            return task.id === id;
+        });
+        if (task == null){
+            return;
+        }
+        task.delete();
+        // JavaScript doesn't include a function to remove a specific item from a pushed array, apparently, so:
+        let index = this.tasks.indexOf(task);
+        this.tasks.splice(index, 1);
+    }
+    saveToStorage() {
+        let storage = window.localStorage;
+        this.tasks.forEach((task) => {
+            task.save();
+            this.taskIds.push(task.id);
+        });
+        storage.setItem("taskIds", JSON.stringify(this.taskIds));
+    }
+    loadFromStorage() {
+        let storage = window.localStorage;
+        let item = storage.getItem("taskIds");
+        console.log(item);
+        this.taskIds = JSON.parse(item);
+        this.tasks = [];
+        this.taskIds.forEach((id) => {
+            const storageItem = storage.getItem(id);
+            console.log(`storageItem: ${storageItem}`);
+            const newObject = JSON.parse(storageItem);
+            console.log(newObject);
+            const newTask = Task.loadFromObject(newObject);
+            this.tasks.push(newTask);
+        });
+    }
+    renderTaskList() {
         let taskListHTML = document.createElement("ul");
         taskListHTML.id = "todo-item-list";
-        this.tasks.forEach((task) => { 
+        this.tasks.forEach((task) => {
             taskListHTML.appendChild(task.render())
         })
         return taskListHTML;
