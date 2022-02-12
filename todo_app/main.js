2c0010b1cac55775204c7f4444c95c6e377c1c93
Joel Jossie <joeljossie@gmail.com>
2022-02-11 18:57:46 -0700

    slight improvement to filters but still buggy

--- todo_app/main.js
+++ todo_app/main.js
@@ -4,21 +4,25 @@ import {TaskList} from "./todos.js"
 let taskList = new TaskList();
 const todoUlElement = document.getElementById("todo-item-list");
 window.addEventListener('load', (event) => {
-    taskList.renderTaskList(todoUlElement, 'all');
-    taskList.registerCallbacks();
-
+    refresh("all");
+    
     const allTasksButton = document.getElementById("all");
     const activeTasksButton = document.getElementById("active");
     const completedTasksButton = document.getElementById("completed");
     
     allTasksButton.addEventListener('click', () => {
-        taskList.renderTaskList(todoUlElement, 'all');
+        refresh("all");
     });
     activeTasksButton.addEventListener('click', () => {
-        taskList.renderTaskList(todoUlElement, 'active');
+        refresh("active");
     });
     completedTasksButton.addEventListener('click', () => {
-        taskList.renderTaskList(todoUlElement, 'completed');
+        refresh("completed")
     });
 });
+
+function refresh(filter){
+    taskList.renderTaskList(todoUlElement, filter);
+    taskList.registerCallbacks();
+}
     
