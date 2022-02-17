cdd10afe69b8387cfd4d7fdf5eda96a4b4d00028
Joel Jossie <joeljossie@gmail.com>
2022-02-17 11:31:22 -0700

    tweak for prod test

--- todo_app/main.js
+++ todo_app/main.js
@@ -4,8 +4,10 @@ import {TaskList} from "./todos.js"
 let taskList = new TaskList();
 const todoUlElement = document.getElementById("todo-item-list");
 window.addEventListener('load', (event) => {
+    console.log("Page Loaded. Testing LocalStorage...");
+    window.localStorage.setItem("test", "This is a test string saved in localStorage");
+    console.log(window.localStorage.getItem("test"));
     refresh("all");
-    
     const allTasksButton = document.getElementById("all");
     const activeTasksButton = document.getElementById("active");
     const completedTasksButton = document.getElementById("completed");
